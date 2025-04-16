ffa29d6fa711f6376406d13ea76b9fabeb791b66
DingoOz <dingo.australia@gmail.com>
2025-04-16 15:03:19 +1000

    chore: updated gitignore

--- build/simple_networking/ptr-wrapper/ptr_wrapper-config.cmake
+++
@@ -1,1 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/ptr_wrapper_targets.cmake)
--- build/simple_networking/simple_networking-config.cmake
+++
@@ -1,3 +0,0 @@
-find_package(fd_wrapper REQUIRED)
-find_package(ptr_wrapper REQUIRED)
-include(${CMAKE_CURRENT_LIST_DIR}/simple_networking_targets.cmake)
